59b3f16b62994c577e48c5ba64d12cdcc32d6408
Mutasem <mutasemshboul14@gmail.com>
2024-07-27 14:36:38 +0300

    Add project files.

---
+++ MVC-Project-Orange/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MVC_Project_Orange.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
