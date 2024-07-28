62c6632269a8e8e091468618d7d138ccce916d54
Mutasem <mutasemshboul14@gmail.com>
2024-07-28 16:54:48 +0300

    Edit Admin Layout

---
+++ MVC-Project-Orange/Controllers/AdminController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MVC_Project_Orange.Data;
+
+namespace MVC_Project_Orange.Controllers
+{
+    public class AdminController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public AdminController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public async Task<IActionResult> ManageProducts()
+        {
+            var applicationDbContext = _context.Products.Include(p => p.Category);
+            return View(await applicationDbContext.ToListAsync());
+        }
+    }
+}
